ac1ef5c564ffd178dbf22df453182e13f2230371
Duncan Temple Lang <duncan@r-project.org>
2022-05-24 12:01:33 -0700

    code to scrape SO result pages and find next page

---
+++ Lectures/Scrape/SOScrape.R
@@ -0,0 +1,42 @@
+
+getStatsExchange =
+function(baseURL = "https://stats.stackexchange.com/questions?tab=newest&pagesize=50",
+         con = getCurlHandle(followlocation = TRUE), maxNum = Inf)
+{
+
+    txt = getURLContent(baseURL, curl = con)
+    doc = htmlParse(txt)
+
+    ans = processResults(doc, con, baseURL)
+    
+    while(length(ans) < maxNum & length(nxt <- getNextPage(doc, baseURL)) ) {
+
+        txt = getURLContent(nxt, curl = con)
+        doc = htmlParse(txt)
+
+        tmp = processResults(doc, con, baseURL)
+        ans = append(ans, tmp)
+
+    }
+
+    ans
+}
+
+
+processResults =
+function(doc, con, baseURL)
+{
+    urls = getNodeSet(doc, "//div[starts-with(@id, 'question-summary') and @data-post-id]//h3/a[@class='s-link']/@href")
+}
+
+
+getNextPage =
+function(doc, baseURL)
+{
+    nxt = getNodeSet(doc, "//a[@rel  = 'next']/@href")
+    if(length(nxt) == 0)
+        return(character())
+    
+    getRelativeURL(nxt[[1]], baseURL)
+}
+
